b264f35d14196ae38e6b1e4a68ed9d54ac3ef27b
Hanna Seithe <hannaseithe@googlemail.com>
2021-05-24 23:11:46 +0200

    Interface for random walker WIP

---
+++ helper.js
@@ -0,0 +1,44 @@
+function Binding(b) {
+    var _this = this;
+    this.elementBindings = [];
+    this.value = b.object[b.property];
+    this.type = typeof this.value;
+    this.valueGetter = function(){
+        return _this.value;
+    }
+    this.valueSetter = function(val){
+        _this.value = val
+        for (var i = 0; i < _this.elementBindings.length; i++) {
+            var binding=_this.elementBindings[i]
+            binding.element[binding.attribute] = val
+        }
+    }
+    this.addBinding = function(element, attribute, event){
+        var binding = {
+            element: element,
+            attribute: attribute
+        }
+        if (event){
+            element.addEventListener(event, function(event){
+                var val = element[attribute];
+                switch (_this.type) {
+                    case "number":
+                        val = Number(element[attribute]);
+                        break;
+                }
+                _this.valueSetter(val);
+            })
+            binding.event = event
+        }       
+        this.elementBindings.push(binding)
+        element[attribute] = _this.value
+        return _this
+    }
+
+    Object.defineProperty(b.object, b.property, {
+        get: this.valueGetter,
+        set: this.valueSetter
+    }); 
+
+    b.object[b.property] = this.value;
+}
--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let x_init, y_init;
 let counter = 0;
 let canvas, ctx;
-let inc = 10;
-var frequency = .0001;
 let noiseFreq = 0.05;
 let p5;
 let rold = 0;
 let line = []
+
+var init = {
+    frequency: 0.00005,
+    inc: 3,
+    lineWidth: 1,
+    opacity: 0.1
+}
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -36,21 +41,21 @@ function crossLine(line1, line2) {
         line2.x1, line2.y1, line2.x2, line2.y2)
 }
 
-function searchForArray(haystack, needle){
+function searchForArray(haystack, needle) {
     var i, j, current;
-    for(i = 0; i < haystack.length; ++i){
-      if(needle.length === haystack[i].length){
-        current = haystack[i];
-        for(j = 0; j < needle.length && needle[j] === current[j]; ++j);
-        if(j === needle.length)
-          return i;
-      }
+    for (i = 0; i < haystack.length; ++i) {
+        if (needle.length === haystack[i].length) {
+            current = haystack[i];
+            for (j = 0; j < needle.length && needle[j] === current[j]; ++j);
+            if (j === needle.length)
+                return i;
+        }
     }
     return -1;
-  }
+}
 
-function noCrossLine(x,y) {
-    if (searchForArray(line, [x,y]) > 0) {
+function noCrossLine(x, y) {
+    if (searchForArray(line, [x, y]) > 0) {
         return false;
     }
     if (line.length > 1) {
@@ -64,20 +69,39 @@ function noCrossLine(x,y) {
             var curLine = {
                 x1: line[i][0],
                 y1: line[i][1],
-                x2: line[i+1][0],
-                y2: line[i+1][1]
+                x2: line[i + 1][0],
+                y2: line[i + 1][1]
             }
-            if (crossLine(curLine,lastLine)) {
+            if (crossLine(curLine, lastLine)) {
                 return false
             }
         }
     }
-    
+
     return true;
 }
 
 function insideCanvas(x, y) {
     return ((x > 0) && (x < canvas.width + 1) && (y > 0) && (y < canvas.height + 1))
+}
+
+function setUpBindings() {
+    var myInputElement1 = document.getElementById("frequencyInput");
+    var myInputElement2 = document.getElementById("incInput")
+
+
+    new Binding({
+        object: init,
+        property: "frequency"
+    })
+        .addBinding(myInputElement1, "value", "keyup")
+
+    new Binding({
+        object: init,
+        property: "inc"
+    })
+        .addBinding(myInputElement2, "value", "keyup")
+
 }
 
 function initialize() {
@@ -89,79 +113,83 @@ function initialize() {
         x_init = Math.floor(canvas.width / 2);
         y_init = Math.floor(canvas.height / 2);
         line.push([x_init, y_init]);
-        ctx.lineWidth = 3;
+        ctx.lineWidth = 1;
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
         //p5 = new window.p5();
         //window.requestAnimationFrame(drawWalker);
+        setUpBindings();
         drawWalker();
     }
 
 }
 
 function drawWalker() {
-    while (counter < 5000) {
+    // while (counter < 500000) {
 
-        ctx.beginPath();
-        var newX = line[line.length - 1][0];
-        var newY = line[line.length - 1][1];
-        ctx.moveTo(newX, newY);
+    ctx.beginPath();
+    var newX = line[line.length - 1][0];
+    var newY = line[line.length - 1][1];
+    ctx.moveTo(newX, newY);
 
-        var r = getRandomInt(8);
-        r = r > 7 ? (r > 15 ? (rold + 1) % 8 : Math.abs((rold + 7) % 8)) : r;
+    var r = getRandomInt(100);
+    r = r > 7 ? /*(r > 15 ? (rold + 1) % 8 :*/ ((rold + 7) % 8) : r;
 
-        //var r = getNoiseInt(8);
-        switch (r) {
-            case 0: {
-                newX += inc;
-                break;
-            }
-            case 2: {
-                newY += inc;
-                break;
-            }
-            case 4: {
-                newX += -inc;
-                break;
-            }
-            case 6: {
-                newY += -inc;
-                break
-            }
-            case 1: {
-                newX += inc;
-                newY += inc;
-                break;
-            }
-            case 7: {
-                newX += inc;
-                newY += -inc;
-                break;
-            }
-            case 5: {
-                newX += -inc;
-                newY += -inc;
-                break;
-            }
-            case 3: {
-                newX += -inc;
-                newY += inc;
-                break;
-            }
+    //var r = getNoiseInt(8);
+    switch (r) {
+        case 0: {
+            newX += init.inc;
+            break;
         }
-        counter++;
-        if (insideCanvas(newX, newY) && noCrossLine(newX, newY)) {
-            line.push([newX, newY]);
-            ctx.lineTo(newX, newY);
-            var red = Math.sin(frequency * counter + 0) * 127 + 128;
-            var green = Math.sin(frequency * counter + 2) * 127 + 128;
-            var blue = Math.sin(frequency * counter + 4) * 127 + 128;
-            ctx.strokeStyle = "rgba(" + red + "," + green + "," + blue + ",0.3)"
-            ctx.stroke();
+        case 2: {
+            newY += init.inc;
+            break;
         }
-
-        //drawWalker();
+        case 4: {
+            newX += -init.inc;
+            break;
+        }
+        case 6: {
+            newY += -init.inc;
+            break
+        }
+        case 1: {
+            newX += init.inc;
+            newY += init.inc;
+            break;
+        }
+        case 7: {
+            newX += init.inc;
+            newY += -init.inc;
+            break;
+        }
+        case 5: {
+            newX += -init.inc;
+            newY += -init.inc;
+            break;
+        }
+        case 3: {
+            newX += -init.inc;
+            newY += init.inc;
+            break;
+        }
+    }
+    counter++;
+    if (insideCanvas(newX, newY) /*&& noCrossLine(newX, newY)*/) {
+        rold = r;
+        line.push([newX, newY]);
+        ctx.lineTo(newX, newY);
+        var red = Math.sin(init.frequency * counter + 0) * 127 + 128;
+        var green = Math.sin(init.frequency * counter + 2) * 127 + 128;
+        var blue = Math.sin(init.frequency * counter + 4) * 127 + 128;
+        ctx.strokeStyle = "rgba(" + red + "," + green + "," + blue + ",0.5)"
+        ctx.stroke();
     }
 
+    //drawWalker();
+    //}
 
 
-    //window.requestAnimationFrame(drawWalker);
+
+    window.requestAnimationFrame(drawWalker);
 }
